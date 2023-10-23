68d40a228cadc502ad96aca069816f6017483206
Hindebox <125393629+Hindebox@users.noreply.github.com>
2023-10-23 17:05:53 +0200

    Update products.js

--- src/routes/products.js
+++ src/routes/products.js
@@ -11,19 +11,6 @@ router.get("/:productId", ProductController.getProduct);
 // CREATE A PRODUCT
 router.post("/", ProductController.createProduct);
 
-//FIX: is to delete?
-// UPDATE A PRODUCT
-// router.put("/", async (req, res) => {
-//   try {
-//     const product = await Product.findById(req.body.productID);
-//     const user = await User.findById(req.body.userID);
-
-//     user.postedProducts.push(product);
-//     await user.save();
-//     res.json({ usersProducts: user.postedProducts });
-//   } catch (error) {}
-// });
-
 //UPDATE A PRODUCT
 router.put("/:productId", ProductController.updateProduct);
 
