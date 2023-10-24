49ed73066acbe56bdc5004c63c112ffe04581dfe
Hindebox <125393629+Hindebox@users.noreply.github.com>
2023-10-24 12:33:14 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -29,11 +29,11 @@ db.once("open", () => {
 });
 
 // Serve static files (React app)
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "build")));
 
 // Handle client-side routing
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 //Listen to server
